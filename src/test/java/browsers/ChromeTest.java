02881aa3f8687fb1b6c278567d26f30e72ac4c5a
huyzxv123@gmail.com <99983352+huyphamits123y@users.noreply.github.com>
2025-12-26 10:05:01 +0700

    first commit

---
+++ src/test/java/browsers/ChromeTest.java
@@ -0,0 +1,27 @@
+package browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ChromeTest {
+    @Test
+    void openBrowserWithDefaultMode(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+    @Test
+    void openBrowserWithHeadlessMode(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless=new");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+}
